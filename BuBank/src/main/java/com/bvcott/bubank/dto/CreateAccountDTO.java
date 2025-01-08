7d62b50d4c5da460e506efdade709ce5e972184d
edgar.afonso <edgar.afonso@fdmgroup.com>
2025-01-08 10:59:07 +0000

    updating configurations so it works in IntelliJ.

--- BuBank/src/main/java/com/bvcott/bubank/dto/CreateAccountDTO.java
+++ BuBank/src/main/java/com/bvcott/bubank/dto/CreateAccountDTO.java
@@ -4,8 +4,9 @@ import java.math.BigDecimal;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
-@Data @Builder
+@Data @Builder @Getter
 public class CreateAccountDTO {
     private Long userId;
     private BigDecimal initialBalance;
