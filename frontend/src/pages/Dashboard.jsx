89489befbd6e53a5e38e81004397431ababb6247
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-01-11 11:54:13 +0000

    Included logout button in dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,32 @@
 import AccountList from "../components/AccountList";
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import {Button} from "react-bootstrap";
+import authService from "../services/authService";
 
 const Dashboard = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        authService.logout(navigate);
+    }
+
     return <div>
         <h1>Dashboard page.</h1>
         <AccountList />
-        <Link to="/create-account" className="btn btn-primary">
-            Create Account
-        </Link>
+        <div className="mt-3 d-flex gap-3">
+            <Button
+                variant="primary"
+                onClick={() => navigate("/create-account")}
+            >
+                Create Account
+            </Button>
+            <Button
+            variant="primary"
+            onClick={handleLogout}
+            >
+                Logout
+            </Button>
+        </div>
     </div>
 }
 
