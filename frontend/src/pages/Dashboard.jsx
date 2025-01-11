496d487e35a0563d2fac26902dd88ecef92439bd
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-01-11 10:13:24 +0000

    Included button from dashboard to direct to create account.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,13 @@
 import AccountList from "../components/AccountList";
+import {Link} from "react-router-dom";
 
 const Dashboard = () => {
     return <div>
         <h1>Dashboard page.</h1>
         <AccountList />
+        <Link to="/create-account" className="btn btn-primary">
+            Create Account
+        </Link>
     </div>
 }
 
