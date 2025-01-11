cb818a32b76e01e98d0925d9766934ce5f6aaf07
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-01-11 16:56:35 +0000

    findByAccountNumber query added

--- BuBank/src/main/java/com/bvcott/bubank/repository/TransactionRepository.java
+++ BuBank/src/main/java/com/bvcott/bubank/repository/TransactionRepository.java
@@ -3,5 +3,8 @@ package com.bvcott.bubank.repository;
 import com.bvcott.bubank.model.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByAccountNumber(String accountNumber);
 }
