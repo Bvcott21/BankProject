2c52cf5ad9ac3edbefa8a9ad40fcaa2a319d5b4a
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-01-12 13:09:41 +0000

    Included service method to create transfers

--- frontend/src/services/txnService.js
+++ frontend/src/services/txnService.js
@@ -4,3 +4,8 @@ export const fetchTransactionsByAccount = async (accountNumber) => {
     const response = await api.get(`/transactions/${accountNumber}`);
     return response.data;
 }
+
+export const createTransaction = async (transactionData) => {
+    const response = await api.post(`/transactions`, transactionData);
+    return response.data;
+}
