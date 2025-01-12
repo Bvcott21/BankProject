73d1446749f35a5f65b6011d4e49b5bb8053fee0
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-01-12 14:24:48 +0000

    Updated repository's query to find receiving transfer transactions.

--- BuBank/src/main/java/com/bvcott/bubank/repository/TransactionRepository.java
+++ BuBank/src/main/java/com/bvcott/bubank/repository/TransactionRepository.java
@@ -1,11 +1,15 @@
 package com.bvcott.bubank.repository;
 
 import com.bvcott.bubank.model.transaction.Transaction;
+import com.bvcott.bubank.model.transaction.TransferTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     List<Transaction> findByAccountNumber(String accountNumber);
-    List<Transaction> findByReceivingAccountNumber(String receivingAccountNumber);
+    @Query("SELECT t FROM TransferTransaction t WHERE t.receivingAccountNumber = :receivingAccountNumber")
+    List<TransferTransaction> findByReceivingAccountNumber(@Param("receivingAccountNumber") String receivingAccountNumber);
 }
