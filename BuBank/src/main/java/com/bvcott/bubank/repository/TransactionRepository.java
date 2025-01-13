d7e1ae8ebc8665555bdbf2d93edddd02ac0cf7b0
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-01-13 12:00:44 +0000

    Additional query to find latest transaction.

--- BuBank/src/main/java/com/bvcott/bubank/repository/TransactionRepository.java
+++ BuBank/src/main/java/com/bvcott/bubank/repository/TransactionRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     List<Transaction> findByAccountNumber(String accountNumber);
     @Query("SELECT t FROM TransferTransaction t WHERE t.receivingAccountNumber = :receivingAccountNumber")
     List<TransferTransaction> findByReceivingAccountNumber(@Param("receivingAccountNumber") String receivingAccountNumber);
+    Optional<Transaction> findTopByOrderByTransactionIdDesc();
 }
