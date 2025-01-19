1fad42380632e7b35fe84a31227a3ec2171ae5fc
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-01-19 17:26:48 +0000

    Corrected Account Repository

--- BuBank/src/main/java/com/bvcott/bubank/repository/AccountRepository.java
+++ BuBank/src/main/java/com/bvcott/bubank/repository/AccountRepository.java
@@ -8,10 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bvcott.bubank.model.account.Account;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    List<Account> findByUser_UserId(Long userId);
+    List<Account> findByCustomer_UserId(Long userId);
     Optional<Account> findByAccountNumber(String accountNumber);
     boolean existsByAccountNumber(String accountNumber);
     Account findTopByOrderByIdDesc();
     Optional<Account> findTopByAccountNumberStartingWithOrderByIdDesc(String prefix);
-    boolean existsByAccountNumberAndUser_Username(String accountNumber, String username);
+    boolean existsByAccountNumberAndCustomer_Username(String accountNumber, String username);
 }
