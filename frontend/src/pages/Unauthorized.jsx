5c13d09ab235e89f653047706f3d001fd18c5c79
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-01-21 14:07:01 +0000

    Updated Unauthorized.jsx's page to reflect correct http status code.

--- frontend/src/pages/Unauthorized.jsx
+++ frontend/src/pages/Unauthorized.jsx
@@ -1,7 +1,7 @@
 const Unauthorized = () => {
     return (
         <div className="text-center mt-5">
-            <h1>401 - Unauthorized</h1>
+            <h1>403 - Unauthorized</h1>
             <p>You do not have permission to access this page.</p>
         </div>
     );
