427845af13ffa7f82ca831c1785d674be885df1d
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-01-21 13:47:00 +0000

    Created AdminDashboard component.

---
+++ frontend/src/pages/AdminDashboard.jsx
@@ -0,0 +1,10 @@
+import AccountCreationRequestList from "../components/account-creation-request/AccountCreationRequestList";
+
+const AdminDashboard = () => {
+    return <div>
+        <h1>Admin Dashboard</h1>
+        <AccountCreationRequestList />
+    </div>
+}
+
+export default AdminDashboard;
