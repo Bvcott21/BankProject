000e17c5f1333174b9ce2f1f401a21a340317833
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-01-21 11:32:30 +0000

    Created controller and endpoint to fetch all account creation requests.

---
+++ BuBank/src/main/java/com/bvcott/bubank/controller/account/creationrequest/AccountCreationRequestController.java
@@ -0,0 +1,27 @@
+package com.bvcott.bubank.controller.account.creationrequest;
+
+import com.bvcott.bubank.model.account.creationrequest.AccountCreationRequest;
+import com.bvcott.bubank.service.account.creationrequest.AccountCreationRequestService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController @RequestMapping("/api/v1/account-requests")
+public class AccountCreationRequestController {
+    private final AccountCreationRequestService requestService;
+
+    public AccountCreationRequestController(AccountCreationRequestService requestService) {
+        this.requestService = requestService;
+    }
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<AccountCreationRequest>> getAllAccountRequests() {
+        List<AccountCreationRequest> requests = requestService.getAllRequests();
+        return ResponseEntity.ok(requests);
+    }
+}
