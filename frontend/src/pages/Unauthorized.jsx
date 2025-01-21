b139b80242f216cad398c72c49b3fd3dc8894f86
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-01-21 13:46:38 +0000

    Created unauthorized page.

---
+++ frontend/src/pages/Unauthorized.jsx
@@ -0,0 +1,10 @@
+const Unauthorized = () => {
+    return (
+        <div className="text-center mt-5">
+            <h1>401 - Unauthorized</h1>
+            <p>You do not have permission to access this page.</p>
+        </div>
+    );
+};
+
+export default Unauthorized;
