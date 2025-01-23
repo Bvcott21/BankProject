90043c6ab00f986c93a246030377876ae9f05df9
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-01-23 17:57:42 +0000

    Updated updateAccountCreationRequest so now accounts are created if the status is approved

--- BuBank/src/main/java/com/bvcott/bubank/service/account/creationrequest/AccountCreationRequestService.java
+++ BuBank/src/main/java/com/bvcott/bubank/service/account/creationrequest/AccountCreationRequestService.java
@@ -1,13 +1,17 @@
 package com.bvcott.bubank.service.account.creationrequest;
 
+import com.bvcott.bubank.dto.CreateAccountDTO;
 import com.bvcott.bubank.dto.account.creationrequest.AccountCreationRequestDTO;
 import com.bvcott.bubank.dto.account.creationrequest.AdminCommentDTO;
 import com.bvcott.bubank.model.account.creationrequest.AccountCreationRequest;
 import com.bvcott.bubank.model.account.creationrequest.AdminComment;
 import com.bvcott.bubank.model.account.creationrequest.RequestStatus;
 import com.bvcott.bubank.model.user.Admin;
+import com.bvcott.bubank.model.user.Customer;
 import com.bvcott.bubank.repository.account.creationrequest.AccountCreationRequestRepository;
 import com.bvcott.bubank.repository.user.AdminRepository;
+import com.bvcott.bubank.repository.user.CustomerRepository;
+import com.bvcott.bubank.service.AccountService;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -17,12 +21,18 @@ import java.util.List;
 public class AccountCreationRequestService {
     private final AccountCreationRequestRepository requestRepo;
     private final AdminRepository adminRepo;
+    private final CustomerRepository customerRepo;
+    private final AccountService accountService;
 
     public AccountCreationRequestService(
             AccountCreationRequestRepository requestRepo,
-            AdminRepository adminRepo) {
+            AdminRepository adminRepo,
+            CustomerRepository customerRepo,
+            AccountService accountService) {
         this.requestRepo = requestRepo;
         this.adminRepo = adminRepo;
+        this.customerRepo = customerRepo;
+        this.accountService = accountService;
     }
 
     public List<AccountCreationRequest> getAllRequests() {
@@ -35,6 +45,10 @@ public class AccountCreationRequestService {
 
         AccountCreationRequest request = requestRepo.findById(requestId)
                 .orElseThrow(() -> new RuntimeException("Account Creation Request not found for id: "+ requestId));
+
+        if(!request.getStatus().equals(RequestStatus.PENDING)) {
+            throw new RuntimeException("Can't comment further on an already closed request.");
+        }
 
         AdminComment comment = new AdminComment();
         comment.setRequest(request);
@@ -55,7 +69,7 @@ public class AccountCreationRequestService {
         return new AccountCreationRequestDTO(request);
     }
 
-    public AccountCreationRequestDTO updateAccountCreationRequestStatus(Long requestId, String username, RequestStatus newStatus) {
+    public AccountCreationRequestDTO updateAccountCreationRequestStatus(Long requestId, String username, RequestStatus newStatus, CreateAccountDTO dto) {
         AccountCreationRequest request = requestRepo.findById(requestId)
                 .orElseThrow(() -> new RuntimeException("Account creation request not found for id: " + requestId));
 
@@ -71,6 +85,11 @@ public class AccountCreationRequestService {
         request.setStatus(newStatus);
 
         request = requestRepo.save(request);
+
+        if(newStatus.equals(RequestStatus.APPROVED)) {
+            accountService.createAccount(dto, request.getRequestedBy().getUsername());
+        }
+
         return new AccountCreationRequestDTO(request);
     }
 }
