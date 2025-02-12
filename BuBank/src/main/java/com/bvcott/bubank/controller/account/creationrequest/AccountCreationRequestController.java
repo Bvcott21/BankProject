a7635674d798a0af702904e9dfa2625766509915
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-02-12 11:27:30 +0000

    REmoved unused import in AccountCreationRequestController.

--- BuBank/src/main/java/com/bvcott/bubank/controller/account/creationrequest/AccountCreationRequestController.java
+++ BuBank/src/main/java/com/bvcott/bubank/controller/account/creationrequest/AccountCreationRequestController.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController @RequestMapping("/api/v1/account-requests")
 @PreAuthorize("hasRole('ROLE_ADMIN')")
