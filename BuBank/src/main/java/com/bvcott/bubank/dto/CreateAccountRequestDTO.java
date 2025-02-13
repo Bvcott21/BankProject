2bf0892823fd05ebb8502e3a110d2a6ae8b7f054
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-02-13 15:36:09 +0000

    Included @Builder on CreateAccountRequestDTO

--- BuBank/src/main/java/com/bvcott/bubank/dto/CreateAccountRequestDTO.java
+++ BuBank/src/main/java/com/bvcott/bubank/dto/CreateAccountRequestDTO.java
@@ -1,9 +1,10 @@
 package com.bvcott.bubank.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Data;
 
-@Data
+@Data @Builder
 public class CreateAccountRequestDTO {
     @NotBlank
     private String accountType;
