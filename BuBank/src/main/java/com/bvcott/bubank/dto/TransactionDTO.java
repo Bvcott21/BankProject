a607b458cd6df39ad437a8372e4a3811a450dce8
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-02-14 10:59:34 +0000

    Included @Builder for TransactionDTO

--- BuBank/src/main/java/com/bvcott/bubank/dto/TransactionDTO.java
+++ BuBank/src/main/java/com/bvcott/bubank/dto/TransactionDTO.java
@@ -5,12 +5,14 @@ import com.bvcott.bubank.model.transaction.merchant.MerchantCategory;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class TransactionDTO {
     @NotBlank(message = "Account Number cannot be null or blank")
     private String accountNumber;
