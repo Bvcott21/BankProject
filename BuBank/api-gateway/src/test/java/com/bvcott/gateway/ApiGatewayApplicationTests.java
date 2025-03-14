2082e85a129d18b02828903443671e60035f5e24
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-03-14 11:17:31 +0000

    Initialize api-gateway module with basic configuration, application properties, and Maven wrapper

---
+++ BuBank/api-gateway/src/test/java/com/bvcott/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bvcott.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
